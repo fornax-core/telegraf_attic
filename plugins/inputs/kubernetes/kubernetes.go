18c43b00ec6d0660080507b6d942436eddae049a
Richie Liming <richard.p.liming@nasa.gov>
2025-01-02 22:44:04 -0500

    working with struct

--- plugins/inputs/kubernetes/kubernetes.go
+++ plugins/inputs/kubernetes/kubernetes.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 	"sync"
 	"time"
@@ -26,26 +27,42 @@ import (
 
 //go:embed sample.conf
 var sampleConfig string
+var invalid_sql_chars, _ = regexp.Compile(`[^_a-z0-9]+`)
 
 const (
 	defaultServiceAccountPath = "/var/run/secrets/kubernetes.io/serviceaccount/token"
 )
 
+type IPAddresses struct {
+	IPv4Internal string
+	IPv4External string
+}
+
+type NodeInfo struct {
+	Addresses IPAddresses
+	Labels map[string]string
+}
+
 // Kubernetes represents the config object for the plugin
 type Kubernetes struct {
-	URL               string          `toml:"url"`
-	BearerToken       string          `toml:"bearer_token"`
-	BearerTokenString string          `toml:"bearer_token_string" deprecated:"1.24.0;1.35.0;use 'BearerToken' with a file instead"`
-	NodeMetricName    string          `toml:"node_metric_name"`
-	LabelInclude      []string        `toml:"label_include"`
-	LabelExclude      []string        `toml:"label_exclude"`
-	ResponseTimeout   config.Duration `toml:"response_timeout"`
-	Log               telegraf.Logger `toml:"-"`
+	URL                     string          `toml:"url"`
+	BearerToken             string          `toml:"bearer_token"`
+	BearerTokenString       string          `toml:"bearer_token_string" deprecated:"1.24.0;1.35.0;use 'BearerToken' with a file instead"`
+	NodeMetricName          string          `toml:"node_metric_name"`
+	LabelInclude            []string        `toml:"label_include"`
+	LabelExclude            []string        `toml:"label_exclude"`
+	ResponseTimeout         config.Duration `toml:"response_timeout"`
+	Log                     telegraf.Logger `toml:"-"`
+	ConvertLabels           bool            `toml:"convert_labels"`
+	IncludeNodeLabels       bool            `toml:"include_node_labels"`
+	IncludeNodeExternalIPv4 bool            `toml:"include_node_external_ipv4"`
 
 	tls.ClientConfig
 
 	labelFilter filter.Filter
 	httpClient  *http.Client
+	Hostname    string
+	NodeInfoMap map[string]NodeInfo
 }
 
 func (*Kubernetes) SampleConfig() string {
@@ -72,22 +89,29 @@ func (k *Kubernetes) Init() error {
 		k.NodeMetricName = "kubernetes_node"
 	}
 
+	if k.IncludeNodeExternalIPv4 {
+		k.Log.Debugf("k.NodeExternalIP true")
+	}
+
+	k.Hostname = os.Getenv("HOSTNAME")
+	k.Log.Debugf("HOSTNAME: %s\n", k.Hostname)
+	k.NodeInfoMap = make(map[string]NodeInfo)
+
 	return nil
 }
 
 func (k *Kubernetes) Gather(acc telegraf.Accumulator) error {
+	k.getNodeInfo()
 	if k.URL != "" {
+		k.Log.Debugf("Gather() k.URL: %s\n", k.URL)
 		acc.AddError(k.gatherSummary(k.URL, acc))
 		return nil
 	}
 
+	k.Log.Debugf("Gather() k.URL empty\n")
 	var wg sync.WaitGroup
-	nodeBaseURLs, err := getNodeURLs(k.Log)
-	if err != nil {
-		return err
-	}
 
-	for _, url := range nodeBaseURLs {
+	for url := range k.NodeInfoMap{
 		wg.Add(1)
 		go func(url string) {
 			defer wg.Done()
@@ -99,53 +123,85 @@ func (k *Kubernetes) Gather(acc telegraf.Accumulator) error {
 	return nil
 }
 
-func getNodeURLs(log telegraf.Logger) ([]string, error) {
+func (k *Kubernetes) getNodeInfo() error {
+
+	var nodes *v1.NodeList
+
 	cfg, err := rest.InClusterConfig()
 	if err != nil {
-		return nil, err
+		return err
 	}
 	client, err := kubernetes.NewForConfig(cfg)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	nodes, err := client.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		return nil, err
-	}
+	if k.URL != "" {
+		k.Log.Debugf("getNodeInfo k.URL not blank, get one host: %s\n", k.Hostname)
+		fieldselector := fmt.Sprintf("metadata.name=%s", k.Hostname)
+		nodes, err = client.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{
+			FieldSelector: fieldselector,
+		})
+		if err != nil {
+			return err
+		}
+	}else {
+		nodes, err = client.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
+		k.Log.Debugf("getNodeInfo k.URL BLANK , get all nodes\n")
+		if err != nil {
+			return err
+		}
+	}	
 
-	nodeUrls := make([]string, 0, len(nodes.Items))
 	for i := range nodes.Items {
+		k.Log.Debugf("Walk nodes: %d\n", i)
 		n := &nodes.Items[i]
-
-		address := getNodeAddress(n.Status.Addresses)
-		if address == "" {
-			log.Warnf("Unable to node addresses for Node %q", n.Name)
+		nodeInfo := NodeInfo{}
+		nodeInfo.Addresses = getNodeAddresses(n.Status.Addresses, k.Log)
+		k.Log.Debugf("Host: %s, Name: %s, Addresses: %+v\n", k.Hostname, n.Name, nodeInfo.Addresses)
+		url := "https://"+nodeInfo.Addresses.IPv4Internal+":10250"
+		nodeInfo.Labels = n.GetLabels()
+		if nodeInfo.Addresses.IPv4Internal == "" {
+			k.Log.Warnf("Unable to get node addresses for Node %q", n.Name)
 			continue
 		}
-		nodeUrls = append(nodeUrls, "https://"+address+":10250")
+		if nodeInfo.Addresses.IPv4External != "" {
+				k.Log.Debugf("Host: %s, have v4external: %s\n", k.Hostname, nodeInfo.Addresses.IPv4External)
+			if k.IncludeNodeExternalIPv4 {
+				k.Log.Debugf("Add  %s to labels\n", nodeInfo.Addresses.IPv4External)
+				nodeInfo.Labels["node_external_ipv4"] = nodeInfo.Addresses.IPv4External
+			}
+		}
+		k.Log.Debugf("getNodeInfo(): Host: %s, Node: %s, url: %s, %d labels\n", k.Hostname, n.Name, url, len(nodeInfo.Labels))
+		k.NodeInfoMap[url] = nodeInfo
 	}
-
-	return nodeUrls, nil
+	k.Log.Debugf("getNodeInfo() Done\n")
+	return nil
 }
 
-// Prefer internal addresses, if none found, use ExternalIP
-func getNodeAddress(addresses []v1.NodeAddress) string {
-	extAddresses := make([]string, 0)
-	for _, addr := range addresses {
-		if addr.Type == v1.NodeInternalIP {
-			return addr.Address
+func getNodeAddresses(naddresses []v1.NodeAddress, log telegraf.Logger) IPAddresses {
+	addresses := IPAddresses{}
+    for _, addr := range naddresses {
+        if addr.Type == v1.NodeInternalIP {
+			if addresses.IPv4Internal != "" {
+				continue
+			}else {
+				addresses.IPv4Internal = addr.Address
+			}
+        }
+		if addr.Type == v1.NodeExternalIP {
+			if addresses.IPv4External != "" {
+				continue
+			}else {
+				addresses.IPv4External = addr.Address
+			}
 		}
-		extAddresses = append(extAddresses, addr.Address)
-	}
-
-	if len(extAddresses) > 0 {
-		return extAddresses[0]
-	}
-	return ""
+    }
+    return addresses
 }
 
 func (k *Kubernetes) gatherSummary(baseURL string, acc telegraf.Accumulator) error {
+	k.Log.Debugf("gatherSummary() baseURL: %s\n", baseURL)
 	summaryMetrics := &summaryMetrics{}
 	err := k.loadJSON(baseURL+"/stats/summary", summaryMetrics)
 	if err != nil {
@@ -157,7 +213,7 @@ func (k *Kubernetes) gatherSummary(baseURL string, acc telegraf.Accumulator) err
 		return err
 	}
 	buildSystemContainerMetrics(summaryMetrics, acc)
-	buildNodeMetrics(summaryMetrics, acc, k.NodeMetricName)
+	k.buildNodeMetrics(baseURL, summaryMetrics, acc)
 	buildPodMetrics(summaryMetrics, podInfos, k.labelFilter, acc)
 	return nil
 }
@@ -184,10 +240,30 @@ func buildSystemContainerMetrics(summaryMetrics *summaryMetrics, acc telegraf.Ac
 	}
 }
 
-func buildNodeMetrics(summaryMetrics *summaryMetrics, acc telegraf.Accumulator, metricName string) {
+func (k *Kubernetes) buildNodeMetrics (baseURL string, summaryMetrics *summaryMetrics, acc telegraf.Accumulator) {
+
 	tags := map[string]string{
 		"node_name": summaryMetrics.Node.NodeName,
 	}
+	var converted string
+	if k.IncludeNodeLabels {
+		k.Log.Debugf("Host: %s, k.NodeLabels true, baseURL: %s", k.Hostname, baseURL)	
+		labels := k.NodeInfoMap[baseURL].Labels
+		k.Log.Debugf("Host: %s, %d labels for baseURL: %s\n", k.Hostname, len(labels), baseURL)
+		for labelk, labelv := range labels {
+			k.Log.Debugf("buildNodeMetrics(): label: %s -> %s", labelk, labelv)
+			if k.labelFilter.Match(labelk) {
+				k.Log.Debugf("buildNodeMetrics(): filter matched: %s -> %s", labelk, labelv)
+				if k.ConvertLabels {
+					converted = invalid_sql_chars.ReplaceAllString(labelk, "_") 
+					tags[converted] = labelv
+				}else {
+					tags[labelk] = labelv
+				}
+			}
+		}
+	}
+
 	fields := make(map[string]interface{})
 	fields["cpu_usage_nanocores"] = summaryMetrics.Node.CPU.UsageNanoCores
 	fields["cpu_usage_core_nanoseconds"] = summaryMetrics.Node.CPU.UsageCoreNanoSeconds
@@ -207,8 +283,9 @@ func buildNodeMetrics(summaryMetrics *summaryMetrics, acc telegraf.Accumulator, 
 	fields["runtime_image_fs_available_bytes"] = summaryMetrics.Node.Runtime.ImageFileSystem.AvailableBytes
 	fields["runtime_image_fs_capacity_bytes"] = summaryMetrics.Node.Runtime.ImageFileSystem.CapacityBytes
 	fields["runtime_image_fs_used_bytes"] = summaryMetrics.Node.Runtime.ImageFileSystem.UsedBytes
-	acc.AddFields(metricName, fields, tags)
+	acc.AddFields(k.NodeMetricName, fields, tags)
 }
+
 
 func (k *Kubernetes) gatherPodInfo(baseURL string) ([]item, error) {
 	var podAPI pods
